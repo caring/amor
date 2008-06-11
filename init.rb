6dad909510e0593dbd532880fd1f6e328ef7aa48
Tapajós <tapajos@gmail.com>
2008-06-11 20:51:35 -0300

    Tests folder.

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
-%w(another_markdown_on_rails).each {|req| require req}
+require 'another_markdown_on_rails'
 
 if Rails::VERSION::STRING < '2.1.0'
   ActionView::Base.register_template_handler('text', AnotherMarkdownOnRails)
 else
   ActionView::Template.register_template_handler('text', AnotherMarkdownOnRails)
 end
-
 
 AnotherMarkdownOnRails::map_headings_down_by 0
 
