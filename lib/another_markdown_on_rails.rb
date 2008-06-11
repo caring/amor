2fd1f36c0b15db6028204bddd81807b4eabc69f1
Tapajós <tapajos@gmail.com>
2008-06-11 20:52:20 -0300

    Set VERSION.

--- lib/another_markdown_on_rails.rb
+++ lib/another_markdown_on_rails.rb
@@ -66,8 +66,8 @@ class AnotherMarkdownOnRails
 
   module VERSION
     MAJOR = 0
-    MINOR = 3
-    TINY  = 0
+    MINOR = 0
+    TINY  = 1
     STRING = [MAJOR, MINOR, TINY].join('.')          
   end
 
