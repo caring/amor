60e1cbf52ab9a3eab5c19c86964a5cb736cb486c
Tapajós <tapajos@gmail.com>
2008-10-01 14:18:24 -0300

    Adding Rails 2.1.1 support.

--- init.rb
+++ init.rb
@@ -1,9 +1,5 @@
 require 'another_markdown_on_rails'
 
-if Rails::VERSION::STRING < '2.1.0'
-  ActionView::Base.register_template_handler('text', AnotherMarkdownOnRails)
-else
-  ActionView::Template.register_template_handler('text', AnotherMarkdownOnRails)
-end
+ActionView::Template.register_template_handler('text', AnotherMarkdownOnRails)
 
 AnotherMarkdownOnRails::map_headings_down_by 0
--- lib/another_markdown_on_rails.rb
+++ lib/another_markdown_on_rails.rb
@@ -38,25 +38,28 @@ class AnotherMarkdownOnRails
   def initialize(view)
     @view = view
   end
-  
+  def compilable?
+    false
+  end
   def self.map_headings_down_by(mapping)
     @@heading_mapping = mapping
   end
   
-  def render(template, local_assigns) 
+  def render template
+    local_assigns = template.locals
     assigns = @view.assigns.dup
   
     if content_for_layout = @view.instance_variable_get("@content_for_layout")
       assigns['content_for_layout'] = content_for_layout
     end
     result = @view.instance_eval do
-		  assigns.each do |key,val|
-		    instance_variable_set "@#{key}", val
-	    end
-		  local_assigns.each do |key,val|
-	  		class << self; self; end.send(:define_method,key) { val }
-			end
-      ERB.new(template, nil, '-').result(binding)
+        assigns.each do |key,val|
+          instance_variable_set "@#{key}", val
+        end
+        local_assigns.each do |key,val|
+          class << self; self; end.send(:define_method,key) { val }
+        end
+      ERB.new(template.source, nil, '-').result(binding)
     end
     
     doc = RDiscount::new(result)
